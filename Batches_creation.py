f3af2a5bcde4b2eb2b0e96383aa0f69498a79509
gabrielhdsc <gabrielhdsc@outlook.com>
2026-03-26 20:36:31 +0000

    fix: batch 2 added with extra column
    New column added to test the _rescued_data autoloader column

--- Batches_creation.py
+++ Batches_creation.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
-df_listings = spark.read.csv("/Volumes/projeto_teste/bronze_teste/landingzone/listings.csv", header=true, infesSchema=True)
-df_past_rates = spark.read.csv("/Volumes/projeto_teste/bronze_teste/landingzone/past_rates.csv", header=true, infesSchema=True)
+df_listings = spark.read.csv("/Volumes/projeto_teste/bronze_teste/landingzone/listings.csv", header=True)
+df_past_rates = spark.read.csv("/Volumes/projeto_teste/bronze_teste/landingzone/past_rates.csv", header=True)
 
 #Salvar apenas uma parte do dataset para funcionamento do autoloader na bronze
 batch_listings_1 = df_listings.limit(1000)
@@ -8,22 +8,35 @@ batch_path_rates_1 = df_past_rates.limit(1000)
 
 
 (batch_listings_1.write
-.format("json")
-.mode("append")
-.save("/Volumes/projeto_teste/bronze_teste/landingzone/json_listings/"))
+    .format("json")
+    .mode("append")
+    .save("/Volumes/projeto_teste/bronze_teste/landingzone/json_listings/"))
 
 (batch_path_rates_1.write
-.format("json")
-.mode("append")
-.save("/Volumes/projeto_teste/bronze_teste/landingzone/json_past_rates/"))
+    .format("json")
+    .mode("append")
+    .save("/Volumes/projeto_teste/bronze_teste/landingzone/json_past_rates/"))
 
 
 print("Dados da leva 1 salvos")
 
 # COMMAND ----------
 
-batch_listings_2 = df_listings.exceptAll(batch_listings_1).limit(1000)
-batch_path_rates_2 = df_past_rates.exceptAll(batch_path_rates_1).limit(1000)
+from pyspark.sql.functions import lit
+
+df_listings = spark.read.csv("/Volumes/projeto_teste/bronze_teste/landingzone/listings.csv", header=True)
+df_past_rates = spark.read.csv("/Volumes/projeto_teste/bronze_teste/landingzone/past_rates.csv", header=True)
+
+
+listings_salvos = spark.read.json("/Volumes/projeto_teste/bronze_teste/landingzone/json_listings")
+past_rates_salvos = spark.read.json("/Volumes/projeto_teste/bronze_teste/landingzone/json_past_rates")
+
+batch_listings_2 = df_listings.exceptAll(listings_salvos).limit(1000)
+path_rates_2 = df_past_rates.exceptAll(past_rates_salvos).limit(1000)
+
+
+#Adiciona coluna extra ao schema para testar o "rescue" do autoloader
+batch_path_rates_2 = path_rates_2.withColumn("extra_tax", lit("unespected column"))
 
 
 (batch_listings_2.write
